6e58b9abb6baa8e645a8b3e9fede4fedb2f5880d
alantop <alantop@qq.com>
2022-11-08 19:27:27 +0800

    add 命令行支持

--- Qt6_sample/boot/main.cpp
+++ Qt6_sample/boot/main.cpp
@@ -1,129 +1,146 @@
-﻿#include <QApplication>
-#include <QSqlDatabase>
+﻿#include "connection.h"
+#include <QApplication>
 #include <QDebug>
-#include <QStringList>
-#include "connection.h"
-#include <QVariant>
+#include <QDir>
 #include <QFileInfo>
 #include <QProcess>
-#include <QDir>
+#include <QSqlDatabase>
+#include <QStringList>
 #include <QTest>
+#include <QVariant>
 
-//cmd
-//on_pushButton_clear_clicked();
+#include <QThread>
 
-//QAbstractItemModel *Imodel=ui->tableView->model();
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QString>
 
-//QModelIndex Iindex = Imodel->index(index.row(),2);//index.row()为算选择的行号。1为所选中行的第一列。。
+#include <QDateTime>
 
-//QVariant datatemp=Imodel->data(Iindex);
-//QString cmd=datatemp.toString();//name即为所选择行的第一列的值。。。
-//qDebug() << cmd;
+#include <QColor>
+#include <QPalette>
 
+#include <QFileInfo>
+#include <QProcess>
 
+#include <windows.h>
 
+#include <QDesktopServices>
 
+#include <QSettings>
 
-//QDetachableProcess process;
-//QString program = "cmd.exe";
-//QStringList arguments = QStringList() << "/K" << cmd;
-//process.setCreateProcessArgumentsModifier([](QProcess::CreateProcessArguments *args)
+#include <QAction>
+#include <QTextBlock>
+
+#include <QMessageBox>
+#include <QPalette>
+#include <QThread>
+#include <QTime>
+
+// cmd
+
+// QDetachableProcess process;
+// QString program = "cmd.exe";
+// QStringList arguments = QStringList() << "/K" << cmd;
+// process.setCreateProcessArgumentsModifier([](QProcess::CreateProcessArguments
+// *args)
 //{
-//    args->flags |= CREATE_NEW_CONSOLE;
-//    args->startupInfo->dwFlags &=~ STARTF_USESTDHANDLES;
-//});
-//process.start(program, arguments);
-//process.detach();
+//     args->flags |= CREATE_NEW_CONSOLE;
+//     args->startupInfo->dwFlags &=~ STARTF_USESTDHANDLES;
+// });
+// process.start(program, arguments);
+// process.detach();
 
+class QDetachableProcess : public QProcess {
+public:
+  QDetachableProcess(QObject *parent = 0) : QProcess(parent) {}
+  void detach() {
+    waitForStarted();
+    setProcessState(QProcess::NotRunning);
+  }
+};
 
+void boot(QString app, int delay_time, int type) {
+  if (type == 1) {
+    QDetachableProcess process;
+    QString program = "cmd.exe";
+    QStringList arguments = QStringList() << "/K" << app;
+    process.setCreateProcessArgumentsModifier(
+        [](QProcess::CreateProcessArguments *args) {
+          args->flags |= CREATE_NEW_CONSOLE;
+          args->startupInfo->dwFlags &= ~STARTF_USESTDHANDLES;
+        });
+    process.start(program, arguments);
+    process.detach();
+    QTest::qSleep(1000 * delay_time);
 
-//class QDetachableProcess
-//        : public QProcess {
-//public:
-//    QDetachableProcess(QObject *parent = 0)
-//        : QProcess(parent) {
-//    }
-//    void detach() {
-//       waitForStarted();
-//       setProcessState(QProcess::NotRunning);
-//    }
-//};
+  }
 
-
-
-bool boot(QString app, int delay_time, int type)
-{
+  if (type == 2) {
     QFileInfo fileInfo(app);
-    if(!fileInfo.isFile())
-      {
-        qDebug() << ("file is not exist.") << Qt::endl;
-        return false;
-      }
+    if (!fileInfo.isFile()) {
+      qDebug() << ("file is not exist.") << Qt::endl;
+    }
 
+      QProcess *myProcess = new QProcess();
 
-    QProcess *myProcess = new QProcess();
+      QFileInfo fileinfo = QFileInfo(app);
 
-    QFileInfo fileinfo = QFileInfo(app);
+      QString filepath = fileinfo.absolutePath();
 
-    QString filepath = fileinfo.absolutePath();
+      qDebug() << QDir::currentPath();
 
-
-    qDebug() << QDir::currentPath();
-
-    qDebug() << "filepath = " << filepath;
-
-
+      qDebug() << "filepath = " << filepath;
 
       QString oldCurrent = QDir::currentPath();
       QDir::setCurrent(filepath);
 
       qDebug() << "oldCurrent=" << oldCurrent;
 
+      qDebug() << QDir::currentPath();
 
-     qDebug() << QDir::currentPath();
+      QString cmd = "\"" + app + "\"";
+      qDebug() << cmd;
 
+      myProcess->startDetached(cmd);
 
-    QString cmd = "\"" + app + "\"";
-    qDebug() << cmd;
+      qDebug() << "setCurrent=" << QDir::setCurrent(oldCurrent);
 
-
-    myProcess->startDetached(cmd);
-
-    qDebug() << "setCurrent=" << QDir::setCurrent(oldCurrent);
-    QTest::qSleep(1000 * delay_time);
+      QTest::qSleep(1000 * delay_time);
 
 
-    return true;
+
+  }
 }
 
-int main(int argc, char *argv[])
-{
-   // QApplication a(argc, argv);
+  int main(int argc, char *argv[]) {
+    // QApplication a(argc, argv);
 
     // 创建数据库连接
-    if (!createConnection()) return 1;
+    if (!createConnection())
+      return 1;
 
     // 使用QSqlQuery查询连接1的整张表，先要使用连接名获取该连接
     QSqlDatabase db1 = QSqlDatabase::database("connection1");
     QSqlQuery query1(db1);
     qDebug() << "connection1:";
     query1.exec("select * from boot");
-    while(query1.next())
-    {
-        qDebug() << query1.value(0).toInt() << query1.value(1).toString() << query1.value(2).toInt() << query1.value(3).toInt() ;
+    while (query1.next()) {
+      qDebug() << query1.value(0).toInt() << query1.value(1).toString()
+               << query1.value(2).toInt() << query1.value(3).toInt();
 
-        boot(query1.value(1).toString() , query1.value(2).toInt() , query1.value(3).toInt());
+      boot(query1.value(1).toString(), query1.value(2).toInt(),
+           query1.value(3).toInt());
     }
 
-//    // 使用QSqlQuery查询连接2的整张表
-//    QSqlDatabase db2 = QSqlDatabase::database("connection2");
-//    QSqlQuery query2(db2);
-//    qDebug() << "connection2:";
-//    query2.exec("select * from student");
-//    while(query2.next())
-//    {
-//        qDebug() << query2.value(0).toInt() << query2.value(1).toString();
-//    }
-    //return a.exec();
-}
-
+    //    // 使用QSqlQuery查询连接2的整张表
+    //    QSqlDatabase db2 = QSqlDatabase::database("connection2");
+    //    QSqlQuery query2(db2);
+    //    qDebug() << "connection2:";
+    //    query2.exec("select * from student");
+    //    while(query2.next())
+    //    {
+    //        qDebug() << query2.value(0).toInt() << query2.value(1).toString();
+    //    }
+    // return a.exec();
+  }
